d9a47803a396717a4785641e01cbd3ae362b3781
QUEK <quekszeyang905@gmail.com>
2024-12-11 17:03:20 +0800

    lab

--- registration.php
+++ registration.php
@@ -5,21 +5,23 @@
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-    <h2>Registration Form</h2>
-    <form action="authenticate.php" method="post">
-        <label for="matric">Matric:</label>
-        <input type="text" id="matric" name="matric" required><br><br>
-        <label for="name">Name:</label>
-        <input type="text" id="name" name="name" required><br><br>
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password" required><br><br>
-        <label for="role">Role:</label>
-        <select id="role" name="role" required>
-            <option value="default  ">Please select</option>
-            <option value="lecturer">lecturer</option>
-            <option value="student">student</option>
-        </select><br><br>
-        <input type="submit" value="Submit">
-    </form>
+    <div class="container">
+        <h2 class="center">Registration Form</h2>
+        <form action="authenticate.php" method="post">
+            <label for="matric">Matric:</label>
+            <input type="text" id="matric" name="matric" required><br><br>
+            <label for="name">Name:</label>
+            <input type="text" id="name" name="name" required><br><br>
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required><br><br>
+            <label for="role">Role:</label>
+            <select id="role" name="role" required>
+                <option value="" disabled selected>Please select</option>
+                <option value="lecturer">Lecturer</option>
+                <option value="student">Student</option>
+            </select><br><br>
+            <input type="submit" value="Submit">
+        </form>
+    </div>
 </body>
 </html>
